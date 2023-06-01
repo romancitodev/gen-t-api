9ae48a553c82433f5d89693b84630d738fc433ac
ChirujanoCodding <trdfabris@gmail.com>
2023-06-01 12:02:09 -0300

    core: added `CORS`

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,32 @@ async fn rocket() -> _ {
     }
     rocket::build()
         .attach(database::init())
+        .attach(CORS)
         .mount("/", routes![root, get_gif_id])
+}
+
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
